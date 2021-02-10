5dba50e83f76da7c709f465e36aab94b33f27049
unknown <311219661@idf.il>
2021-02-10 16:51:43 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useRef } from "react";
+import QrReader from "react-qr-reader";
+import { useTorchLight } from "@blackbox-vision/use-torch-light";
 
 function App() {
+  const streamRef = useRef(null);
+
+  const [error, setError] = useState(null);
+  const [data, setData] = useState("No result");
+
+  const [on, toggle] = useTorchLight(streamRef.current);
+
+  const setRef = ({ stream }) => {
+    streamRef.current = stream;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <QrReader
+        onLoad={setRef}
+        onScan={setData}
+        onError={setError}
+        style={{ width: "100%" }}
+      />
+      <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button>
+      <p>{JSON.stringify(data, null, 2)}</p>
+      <p>{JSON.stringify(error, null, 2)}</p>
+    </>
   );
 }
 
