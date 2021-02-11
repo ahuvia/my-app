80817872bbd85a33b6a469aaa95cf03038195a5f
unknown <311219661@idf.il>
2021-02-11 12:58:55 +0200

    o

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useRef, useState } from "react";
 import QrReader from "react-qr-reader";
 import { useTorchLight } from "@blackbox-vision/use-torch-light";
 import Troch from "./Troch";
-
+import logo from "./logo.svg";
+import { ReactFlashlight } from "react-flashlight";
 function App() {
   const streamRef = useRef(null);
 
@@ -14,11 +15,21 @@ function App() {
   const setRef = ({ stream }) => {
     streamRef.current = stream;
   };
+  const style = {
+    backgroundImage: `url(${logo})`,
+    height: "90vh",
+    width: "90vw",
+  };
 
   return (
     <>
-      <Troch />
+      {/* <Troch /> */}
       לא עובד
+      <header className="App-header">
+        <ReactFlashlight>
+          <div style={style}>Hello, I am a child element.</div>
+        </ReactFlashlight>
+      </header>
       {/* <QrReader
         onLoad={setRef}
         onScan={setData}
