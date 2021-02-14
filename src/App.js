e02d860f38180fb228e32d01a154247e32deb2ae
unknown <311219661@idf.il>
2021-02-14 11:44:10 +0200

    a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useRef, useState } from "react";
 import QrReader from "react-qr-reader";
 import { useTorchLight } from "@blackbox-vision/use-torch-light";
 import Troch from "./Troch";
-import Torch2 from "./Torch2";
 
 function App() {
   const streamRef = useRef(null);
@@ -18,18 +17,17 @@ function App() {
 
   return (
     <>
-      <Torch2 />
-      {/* <Troch /> */}
-      {/* לא עובד
+      <Troch />
+
       <QrReader
         onLoad={setRef}
         onScan={setData}
         onError={setError}
         style={{ width: "100%" }}
-      /> */}
-      {/* <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button>
+      />
+      {/* <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button> */}
       <p>{JSON.stringify(data, null, 2)}</p>
-      <p>{JSON.stringify(error, null, 2)}</p> */}
+      <p>{JSON.stringify(error, null, 2)}</p>
     </>
   );
 }
