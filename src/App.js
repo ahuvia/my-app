a676a7abdd24f99932488a9c369a678d74351052
unknown <311219661@idf.il>
2021-02-14 10:55:27 +0200

    a

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
   return (
     <>
       {/* <Troch /> */}
-      לא עובד
+      {/* לא עובד
       <QrReader
         onLoad={setRef}
         onScan={setData}
         onError={setError}
         style={{ width: "100%" }}
-      />
+      /> */}
       <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button>
       <p>{JSON.stringify(data, null, 2)}</p>
       <p>{JSON.stringify(error, null, 2)}</p>
