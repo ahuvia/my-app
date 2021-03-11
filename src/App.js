5ae3e7f23376a77210739861c6c8d751cfa0d919
unknown <311219661@idf.il>
2021-03-11 13:06:54 +0200

    add to lib/getDevicId

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import { useRef, useState } from "react";
 import QrReader from "react-qr-reader";
-import { useTorchLight } from "@blackbox-vision/use-torch-light";
-import Torch from "./Torch";
-import ScanQr from "./camera/ScanQr";
+// import { useTorchLight } from "@blackbox-vision/use-torch-light";
+// import Torch from "./Torch";
+// import ScanQr from "./camera/ScanQr";
 
-import BarcodeScannerComponent from "react-webcam-barcode-scanner2";
+// import BarcodeScannerComponent from "react-webcam-barcode-scanner2";
 import React from "react";
+
 function App() {
-  const [data, setData] = React.useState("Not Found");
+  const [data, setData] = useState("Not Found");
+
+  const handleError = (err) => {
+    console.error(err);
+  };
 
   return (
-    <>
-      <ScanQr />
+    <div>
+      {/* <ScanQr /> */}
       {/* <Torch /> */}
+      <QrReader
+        delay={300}
+        onScan={(data) => setData(data)}
+        onError={handleError(data)}
+        style={{ width: "30%" }}
+      />
+      {data}
 
-      {/* <QrReader
-        onLoad={setRef}
-        onScan={setData}
-        onError={setError}
-        style={{ width: "100%" }}
-      /> */}
       {/* <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button> */}
       {/* <p>{JSON.stringify(data, null, 2)}</p>
       <p>{JSON.stringify(error, null, 2)}</p> */}
@@ -31,8 +37,8 @@ function App() {
           else setData("Not Found");
         }}
       />
-      <p>{data}</p> */}
-    </>
+      // <p>{data}</p> */}
+    </div>
   );
 }
 
