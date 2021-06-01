e15371efa4682692efe4cbee6f7670d137c2bd14
unknown <311219661@idf.il>
2021-06-01 14:13:13 +0300

    zxingBrowser

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useRef, useState } from "react";
 import QrReader from "react-scan-qr";
 // import { useTorchLight } from "@blackbox-vision/use-torch-light";
 // import Torch from "./Torch";
-// import ScanQr from "./camera/ScanQr";
-
+import ScanQr from "./camera/ScanQr";
+import "./icon.css";
 // import BarcodeScannerComponent from "react-webcam-barcode-scanner2";
 import React from "react";
+import ZxingBrower from "./zxingBrower";
 const tiny = require("@ahuvia/tiny");
 
 function App() {
@@ -15,19 +16,22 @@ function App() {
     console.error(err);
   };
 
+  
+
   return (
     <div>
-      {tiny("אהוביה שני")}
+  
+      {/* {tiny("אהוביה שני")} */}
       {/* <ScanQr /> */}
       {/* <Torch /> */}
-      <QrReader
+      {/* <QrReader
         delay={300}
         onScan={(data) => setData(data)}
         onError={handleError(data)}
         style={{ width: "90%" }}
       />
-      {data}
-
+      {data} */}
+<ZxingBrower/>
       {/* <button onClick={toggle}>{on ? "Disable Torch" : "Enable Torch"}</button> */}
       {/* <p>{JSON.stringify(data, null, 2)}</p>
       <p>{JSON.stringify(error, null, 2)}</p> */}
