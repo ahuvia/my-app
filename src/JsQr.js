368dbfd276307e0e27bde9ca9429c479321cdade
Ace-Code <Starfish-race-user1@yatzma.onmicrosoft.com>
2021-06-08 12:51:36 +0300

    zoom

--- src/JsQr.js
+++ src/JsQr.js
@@ -16,7 +16,7 @@ export default function JsQr() {
 
   const webcam = async () => {
     const mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "environment", zoom:5   },
+      video: { facingMode: "environment", zoom:3   },
       //, frameRate: { ideal: 24, max: 30 }
     });
     const videoTag = document.getElementById("videoo");
