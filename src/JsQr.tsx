d0d6c3621ed21c783cd6b1725f7bdfbf85d6c00c
unknown <311219661@idf.il>
2021-08-03 15:08:51 +0300

    add reslutia

--- src/JsQr.tsx
+++ src/JsQr.tsx
@@ -14,7 +14,7 @@ export default function JsQr() {
 
   const webcam = async () => {
     const constraints = {
-      video: { facingMode: "environment", zoom: 3.0,frameRate: 60}
+      video: { facingMode: "environment", zoom: 2.0,width: {ideal: 1920}, height: {ideal: 1080}}
     };
 
     const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
