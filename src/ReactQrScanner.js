2e474d5f873694766977833dcac595754491e980
unknown <311219661@idf.il>
2021-10-28 11:00:33 +0300

    replace to react-qr-scanner

--- src/ReactQrScanner.js
+++ src/ReactQrScanner.js
@@ -18,7 +18,7 @@ export default function ReactQrScanner() {
         delay={delay}
         style={previewStyle}
         onError={(err)=>handleError(err)}
-        facingMode={"rear"}
+        facingMode="rear"
         onScan={(res) => {
           console.log(res)
           if(res)
