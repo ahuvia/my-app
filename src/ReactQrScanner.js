b993690de3ae9a6f13f7e94682c06f9c2250e15d
Ace-Code <Starfish-race-user1@yatzma.onmicrosoft.com>
2021-11-14 11:30:37 +0200

    copy example

--- src/ReactQrScanner.js
+++ src/ReactQrScanner.js
@@ -1,41 +1,48 @@
 import React, { Component, useState } from "react";
 import QrReader from "react-qr-reader";
 
-export default function ReactQrScanner() {
-  const [delay, setdelay] = useState(100);
-  const [result, setresult] = useState("");
-  const rear = 'rear'
-  const handleError = (err)=>{
-      console.error(err)
+export default class ReactQrScanner extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 100,
+      result: 'No result',
+    }
+
+    this.handleScan = this.handleScan.bind(this)
+    this.openImageDialog = this.openImageDialog.bind(this)
   }
-  const previewStyle = {
-    height: 240,
-    width: 240,
+  handleScan(result){
+    if(result){
+      this.setState({ result })
+    }
   }
-  const constraints = {vidoe:{facingMode:"environment"}}
-  return (
-    <div>
-      <h3>React-qr-acanner</h3>
-      <QrReader
-        delay={1}
-        style={previewStyle}
-        facingMode='environment'
-        // constraints={constraints}
-        onError={(err)=>handleError(err)}
-        showViewFinder={false}
-        onScan={(res) => {
-          console.log(res)
-           if(res)
-           setresult(res)
-        }}
-      />
-     <div>
-       <br></br>
-       <br></br>
-       <br></br>
-     <p>{result}</p>
-     <h1>09:50</h1>
-     </div>
-    </div>
-  );
+  handleError(err){
+    console.error(err)
+  }
+  openImageDialog() {
+    this.refs.qrReader1.openImageDialog()
+  }
+
+  render(){
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    }
+
+    return(
+      <div>
+        <QrReader
+          ref="qrReader1"
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          legacyMode
+        />
+        <input type="button" value="Submit QR Code" onClick={this.openImageDialog} />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
 }
