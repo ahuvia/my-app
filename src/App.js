32d0829924af7002b55d26e05697863635cd6c2c
Ace-Code <Starfish-race-user1@yatzma.onmicrosoft.com>
2022-02-16 14:14:03 +0200

    replace to torch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import ReactQrReader from './ReactQrReader'
+import Torch from "./Torch";
 const tiny = require("@ahuvia/tiny");
 
 function App() {
   
   return (
     <div>
-      <ReactQrReader/>
+      {/* <ReactQrReader/> */}
+      <Torch/>
     </div>
   );
 }
