8a36f58b22678070271261a687cef53b9354f92e
Ace-Code <Starfish-race-user1@yatzma.onmicrosoft.com>
2022-03-21 14:43:51 +0200

    add playsiline

--- src/JsQr.tsx
+++ src/JsQr.tsx
@@ -33,8 +33,8 @@ export default function JsQr() {
     videoTag.autoplay = true;
     // videoTag.height = 290;
     // videoTag.width = 290;
-    videoTag.setAttribute("autoplay", "");
-    videoTag.setAttribute("muted", "");
+    videoTag.setAttribute("autoplay", "true");
+    videoTag.setAttribute("muted", "true");
     videoTag.setAttribute("playsinline", "true");
     // if ("srcObject" in videoTag) {
     videoTag.srcObject = mediaStream;
